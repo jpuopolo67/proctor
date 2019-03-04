c1bcc38a2e9afc4e4c5d6c9bc5fb4634b1efbb05
John Puopolo <puopoloj1@wit.edu>
2019-03-04 17:01:55 -0500

    Added human-readable time calculations.

--- proctor.py
+++ proctor.py
@@ -129,22 +129,40 @@ class Proctor:
                     self.logger.warning(f'NO COMMIT. Project found but cannot find a commit.')
             else:
                 self.logger.warning(f'NOT FOUND: {email}. Project not found. Check email address.')
-
             self.logger.info("project due: {}, latest_commit: {}".format(project_due_date, latest_commit_date))
 
-            # Exmaple of what's returned from server: 2019-03-03T23:39:40.000-05:00
-            dt_due = dt.strptime(project_due_date, "%Y-%m-%dT%H:%M:%S%z")
-            latest_commit_date = latest_commit_date[::-1].replace(':', '', 1)[::-1]
-            dt_latest = dt.strptime(latest_commit_date, "%Y-%m-%dT%H:%M:%S.%f%z")
-            date_diff = dt_due - dt_latest
-            total_sec = date_diff.total_seconds()
-            mins = total_sec / 60.0
-            hrs = mins / 60.0
-            days = hrs / 24.0
-
-            parts = {'total': total_sec, 'mins': mins, 'hrs': hrs, 'days': days}
-            self.logger.info(parts)
+            # Calculate time differences and report back in human readable form
+            is_ontime, days, hours, mins = self._get_dt_diff_human_readable(project_due_date, latest_commit_date)
             pass
+
+    def _get_dt_diff_human_readable(self, project_due_date, latest_commit_date):
+
+        # Exmaple of project date returned from server: 2019-03-03T23:39:40.000-05:00
+        dt_due = dt.strptime(project_due_date, "%Y-%m-%dT%H:%M:%S%z")
+
+        # Remove last : so that strptime can parse correcly
+        latest_commit_date = latest_commit_date[::-1].replace(':', '', 1)[::-1]
+        dt_latest = dt.strptime(latest_commit_date, "%Y-%m-%dT%H:%M:%S.%f%z")
+
+        # Get the difference and return a tuple in days, hrs, mins
+        date_diff = dt_due - dt_latest
+        total_sec = date_diff.total_seconds()
+
+        is_ontime = total_sec >= 0
+        total_sec = abs(total_sec)
+        days, hours, mins = self._convert_secs_to_days_hrs_mins(total_sec)
+
+        return (is_ontime, days, hours, mins)
+
+
+    def _convert_secs_to_days_hrs_mins(self, total_sec):
+        days = total_sec // (24 * 3600)
+        total_sec = total_sec % (24 * 3600)
+        hours = total_sec // 3600
+        total_sec %= 3600
+        mins = total_sec // 60
+        return (days, hours, mins)
+
 
     def _get_emails_from_file(self, email_file):
         """Returns a list of emails from the given file."""
